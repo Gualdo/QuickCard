bf298581333f51b3c496e61584e618f8b1146218
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2021-08-16 11:57:48 -0400

    Initial Commit

---
+++ QuickCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickCard
+//
+//  Created by Eduardo David De La Cruz Marrero on 16/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickCard/QuickCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCardApp.swift
+//  QuickCard
+//
+//  Created by Eduardo David De La Cruz Marrero on 16/8/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
