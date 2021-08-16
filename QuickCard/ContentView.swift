380d6d11b90a7d0097a46cf95ec7a47bba07fc69
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2021-08-16 12:50:28 -0400

    All done

--- QuickCard/ContentView.swift
+++ QuickCard/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(Color("CardBackground"))
+                .frame(width: 300, height: 200)
+                .cornerRadius(10)
+                .shadow(radius: 10)
+            VStack(alignment: .leading) {
+                Text("Eduardo De La Cruz")
+                    .font(.title)
+                Text("Hello SwiftUI")
+                    .italic()
+                Spacer()
+                HStack {
+                    Image(systemName: "envelope.fill")
+                        .foregroundColor(.orange)
+                    Text("gualdodelacruz@gmail.com")
+                }
+                HStack {
+                    Image(systemName: "link")
+                        .foregroundColor(.orange)
+                    Text("gualdodelacruz.com")
+                }
+                HStack {
+                    Image(systemName: "location.fill")
+                        .foregroundColor(.orange)
+                    Text("La Tahona")
+                }
+            }
             .padding()
+            .foregroundColor(.white)
+        }
+        .frame(width: 300, height: 200)
     }
 }
 
